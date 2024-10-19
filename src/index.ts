3f1a86d0097b6829faec8c47dcd1c0b1bb84c259
Ifamust Simon <ifamust.s@gmail.com>
2024-10-19 15:49:48 -0400

    added drizzle config

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
-import express from 'express';
+import express, { json, urlencoded } from 'express';
 import productsRouter from './routes/products';
 
 const port = 3000;
 
 const app = express();
+app.use(urlencoded({ extended: false }));
+app.use(json());
 
 app.get('/', (req, res) => {
   res.send('hello world');
